bade38efc730ed75086885c6b132af2f7aedb0b7
hamed <hamedkaregar849@gmail.com>
2024-11-21 08:45:44 +0330

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+for num in range(1,10):
+    if num % 2 == 1:
+        for star in range(1,6):
+            print("*" * star)
+    else :
+        for star in range(5, 0, -1):
+            print("*" * star)
+
+
